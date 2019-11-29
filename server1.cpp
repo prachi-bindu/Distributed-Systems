f3665cf86ed0c6e28137ef7056480ff5fb4edea0
prachi-bindu <bprachi1811@gmail.com>
2019-11-29 13:17:46 -0500

    First commit of hello world

---
+++ server1.cpp
@@ -0,0 +1,16 @@
+#include<iostream.h>
+
+using namespace std;
+
+int main()
+{
+	//create a socket
+	//bind socket ot IP and Port
+	//listen for incoming connection request
+	//close the listener
+	//close the socket
+
+	cout<<"hello";
+	
+	return 0;
+}
